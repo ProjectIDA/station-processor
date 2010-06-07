787f64f0e35dc014e9078e61e48f7ec4339cefff
jdedwards <jdedwards@7e50f963-adc0-43f8-a874-3926c70588ee>
2010-06-07 20:58:48 +0000

    Fixed issue with page content not collapsing with row Added styles for legend
    git-svn-id: svn+ssh://136.177.121.23/svn/asldev/q330asp@283 7e50f963-adc0-43f8-a874-3926c70588ee

--- trunk/web/falcon.js
+++ trunk/web/falcon.js
@@ -3,17 +3,18 @@ function display(id) {
     var els = document.getElementsByTagName('div');
     for (var i in els) {
         if (els[i].className == 'alarms') {
-            if ((els[i].id == id) && (els[i].style.visibility != 'visible')) {
-                els[i].style.visibility = 'visible';
+            if ((els[i].id == id) && (els[i].style.display == 'none')) {
+                els[i].style.display = 'block';
                 els[i].style.height = 'auto';
             } else {
-                els[i].style.visibility = 'hidden';
+                els[i].style.display = 'none';
                 els[i].style.height = '0px';
             }
         }
     }
 }
-function initialize()
-{
+
+function initialize() {
     display('NOTHING');
 }
+
